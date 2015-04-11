f34f1ce2789ce8b98f85dddb78dfed617d07d374
SamuelTai <stimothy77@gmail.com>
2015-04-11 23:14:37 +0800

    1st version of non-recursive fibonacci
    write the non-recursive and the time count

--- lab-1i/fibseq.c
+++ lab-1i/fibseq.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
-
+#include <time.h>
 extern int fibonacci(int x);
 
 int main(int argc, char **argv)
+
 {
   int number=0;
-  int result=0;
-
+  int i=0;  
+  clock_t start_time;
+  clock_t current_time;
+  float time_interval;
+  int a =1;
+  int b = 1;
+  int c;
   scanf("%d",&number);
-  result = fibonacci(number);   
-  printf("The fibonacci sequence at %d is: %d\n", number, result);
-}
+  start_time = clock();
+  	for( i = 1 ;i <= number;i++)
+  	{	
+	  if( i <=2){ 
+		current_time = clock();
+		time_interval = current_time - start_time;
+          	printf("The fibonacci sequence at %d is: %d   %f \n", i,1,time_interval );
+		     }
+	  else{
+	  c = a+b;
+	  b = a  ;
+	  a = c  ;
+	  current_time = clock();
+	  time_interval = current_time - start_time;
+          printf("The fibonacci sequence at %d is: %d   %f \n", i, c,time_interval);}
+  	}	
 
+}
