d6855f88d65e37148ef9a6992c5e8ed05470dd7c
SamuelTai <stimothy77@gmail.com>
2015-04-16 21:39:51 +0800

    I just took a look at many classmates' hackpad to get many help to finish this
    I rewrite a new one

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -9,35 +9,21 @@
 	.type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
-	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	push {r3, r4, r5, r6, lr}
+	add r4, r0, #0
+	mov r5, #0
+	ldr r5,=-1
+	ldr r6,=1
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+.loop:
+	add r0,r5,r6
+	mov r5,r6
+	mov r5,r0
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
-
-	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+	subs r4,r4,#1
+	bne .loop
+	pop {r3,r4,r5,r6,pc}
 
 	.size fibonacci, .-fibonacci
 	.end
